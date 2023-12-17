8019c3554c7ac1fcd9b202efbd30f8f28d7b6359
ncras04 <nicolasaschmitt@googlemail.com>
2023-12-17 12:48:44 +0100

    Point Manager added

--- Assets/PointManager.cs
+++ Assets/PointManager.cs
@@ -1,18 +1,88 @@
+using QuickTime;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
+using Tetris;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PointManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Action<BigInteger> ScoreChanged;
+
+    public BigInteger Points
     {
-        
+        get
+        {
+            return m_points;
+        }
+        set
+        {
+            if (m_points != value)
+            {
+                m_points = value;
+                Debug.Log(m_points);
+                ScoreChanged?.Invoke(m_points);
+            }
+        }
     }
 
-    // Update is called once per frame
+    private BigInteger m_points = 0;
+
+    [SerializeField]
+    private int m_basePoints = 0;
+    [SerializeField]
+    private int baseMultiplier = 1000;
+
+    private int m_deltaAccumulator = 0;
+
+    private bool m_rightKnob = false;
+    private int m_knobPoints = 200;
+
+    private void OnEnable()
+    {
+        FindObjectOfType<RightKnobTemperature>().IsMultiplyChanged += OnRightKnobMultiplyChanged;
+        FindObjectOfType<QuickTimeHandler>().OnQuickTimeEventFinished += OnQuickTimeFinished;
+        FindObjectOfType<BowlManager>().OnBowlFinish.AddListener(OnBowlFinished);
+        FindObjectOfType<TetrisProvider>().OnFullLine += OnFullLine;
+    }
+
+    private void OnDisable()
+    {
+        FindObjectOfType<RightKnobTemperature>().IsMultiplyChanged -= OnRightKnobMultiplyChanged;
+        FindObjectOfType<QuickTimeHandler>().OnQuickTimeEventFinished -= OnQuickTimeFinished;
+        FindObjectOfType<BowlManager>().OnBowlFinish.RemoveListener(OnBowlFinished);
+        FindObjectOfType<TetrisProvider>().OnFullLine -= OnFullLine;
+    }
+
+    private void OnFullLine()
+    {
+        Points += baseMultiplier * baseMultiplier;
+    }
+
+    private void OnBowlFinished(Bowl arg0)
+    {
+        Points += arg0.ParticleAmount * baseMultiplier;
+    }
+
+    private void OnQuickTimeFinished(AQuickTimeEvent _event)
+    {
+        var pawEvent = _event is CatPawnEvent;
+
+        if (pawEvent)
+            Points += pawEvent.GetHashCode();
+    }
+
+    private void OnRightKnobMultiplyChanged(bool _obj)
+    {
+        m_rightKnob = _obj;
+    }
+
     void Update()
     {
-        
+        m_deltaAccumulator = (int)(Time.deltaTime * 1000f);
+
+        Points += m_deltaAccumulator * (Convert.ToInt32(m_rightKnob) * m_knobPoints);
     }
 }
